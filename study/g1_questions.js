fb3fb512d68a45287ea8a983847d7084680d65e0
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-04 23:28:19 -0400

    Create study/g1_questions.js

---
+++ study/g1_questions.js
@@ -0,0 +1,54 @@
+// Subelement G1 – Commission’s Rules
+const g1_questions = [
+    {
+        question: "On which HF and/or MF amateur bands are there portions where General class licensees cannot transmit?",
+        answers: {
+            A: "60 meters, 30 meters, 17 meters, and 12 meters",
+            B: "160 meters, 60 meters, 15 meters, and 12 meters",
+            C: "80 meters, 40 meters, 20 meters, and 15 meters",
+            D: "80 meters, 20 meters, 15 meters, and 10 meters"
+        },
+        correct: "C"
+    },
+    {
+        question: "What is the maximum height above ground for an antenna structure not near a public use airport without requiring notification to the FAA and registration with the FCC?",
+        answers: {
+            A: "50 feet",
+            B: "100 feet",
+            C: "200 feet",
+            D: "250 feet"
+        },
+        correct: "C"
+    },
+    {
+        question: "What is the maximum transmitter power an amateur station may use on 10.140 MHz?",
+        answers: {
+            A: "200 watts PEP output",
+            B: "1000 watts PEP output",
+            C: "1500 watts PEP output",
+            D: "2000 watts PEP output"
+        },
+        correct: "A"
+    },
+    {
+        question: "Volunteer Examiners are accredited by what organization?",
+        answers: {
+            A: "The Federal Communications Commission",
+            B: "The Universal Licensing System",
+            C: "A Volunteer Examiner Coordinator",
+            D: "The Wireless Telecommunications Bureau"
+        },
+        correct: "C"
+    },
+    {
+        question: "The frequency allocations of which ITU region apply to radio amateurs operating in North and South America?",
+        answers: {
+            A: "Region 4",
+            B: "Region 3",
+            C: "Region 2",
+            D: "Region 1"
+        },
+        correct: "C"
+    }
+];
+
